b8f35f8686cc1901e690b20040fc300dc456201b
nvtanh <nvtanh.ptit@gmail.com>
2016-04-13 00:31:44 +0700

    [Feature] update README file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 p "Clear DB"
 User.destroy_all
-Boat.destroy_all
 
 p "Create user"
 user = User.create(email: "test@gmail.com", password: "12345678", password_confirmation: "12345678")
 
 p "Create boats"
 25.times do |i|
-  user.boats.create!(name: "boat#{i+1}", year: 2016, image: File.new(Rails.root.to_s + '/app/assets/images/test.jpg'))
+  boat = user.boats.create!(name: "boat#{i+1}", year: 2016, image: File.new(Rails.root.to_s + '/app/assets/images/test.jpg'))
+  boat.goods.create!(name: "goods of boat #{boat.id}", quantity: i)
 end
 p "End seed"
